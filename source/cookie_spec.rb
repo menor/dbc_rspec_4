98412bce63c0a68901a88259418d303efb09dae4
menor <josemenorgmail.com>
2014-03-11 20:36:03 -0700

    Implement all tests.

--- source/cookie_spec.rb
+++ source/cookie_spec.rb
@@ -22,13 +22,20 @@ describe Cookie do
   end
 
   describe "#type" do
-    it "returns the type of the cookie"
+    it "returns the type of the cookie" do
+      cookie.type.should eq(type)
+    end
   end
 
   describe "#bake!" do
-    it "requires an integer time argument"
+    it "requires an integer time argument" do
+      expect { cookie.bake! }.to raise_error(ArgumentError)
+    end
 
-    it "returns the cookie object"
+    it "returns the cookie object" do
+      baked = cookie.bake!(10)
+      baked.should be_instance_of(Cookie)
+    end
 
     it "changes the status of the cookie when given enough time" do
       expect { cookie.bake!(10) }.to change(cookie, :status)
@@ -36,26 +43,42 @@ describe Cookie do
   end
 
   describe "#status" do
-    it "returns the cookie's current status"
 
-    context "when unbaked" do
-      it "is `:doughy`" 
+    
+    
+      context "when unbaked" do
+        it "is `:doughy`" do
+          expect(cookie.status).to be(:doughy)
+        end
+      end
+
+      context "when baked for less than 7 minutes" do
+        it "is `:doughy`" do
+          baked = cookie.bake!(6)
+          expect(cookie.status).to be(:doughy)
+        end
+      end  
+
+      context "when baked for at least 7 but less than 10 minutes" do
+        it "is `:almost_ready`" do
+          baked = cookie.bake!(8)
+          expect(cookie.status).to be(:almost_ready)
+        end
+      end
+
+      context "when baked for at least 10 but less than 12 minutes" do
+        it "is `:ready`" do 
+          baked = cookie.bake!(11)
+          expect(cookie.status).to be(:ready)
+        end
+      end
+
+      context "when baked for at least 12 minutes" do
+        it "is `:burned`" do
+          baked = cookie.bake!(12)
+          expect(cookie.status).to be(:burned)
+        end
+      end
     end
-
-    context "when baked for less than 7 minutes" do
-      it "is `:doughy`"
-    end  
-
-    context "when baked for at least 7 but less than 10 minutes" do
-      it "is `:almost_ready`"
-    end
-
-    context "when baked for at least 10 but less than 12 minutes" do
-      it "is `:ready`"
-    end
-
-    context "when baked for at least 12 minutes" do
-      it "is `:burned`"
-    end
-  end
+  
 end
